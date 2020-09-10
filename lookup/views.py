d0bb21204310947c7371eef26fee725c49dbadb9
CasperFicke <cficke@quicknet.nl>
2020-09-10 17:19:32 +0200

    add base.html BS and about page

--- lookup/views.py
+++ lookup/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+# home view
+def home(request):
+  return render(request, 'home.html', {})
+
+# about view
+def about(request):
+  return render(request, 'about.html', {})
