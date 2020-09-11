1696e1c287e34724917cd4f85775cd3a8f3a5cbf
CasperFicke <cficke@quicknet.nl>
2020-09-11 17:41:47 +0200

    add bevraging meteoserver

--- lookup/views.py
+++ lookup/views.py
@@ -1,8 +1,20 @@
 from django.shortcuts import render
 
+import os
+
 # home view
 def home(request):
-  return render(request, 'home.html', {})
+  import json
+  import requests
+  meteoserver_apikey = os.getenv('METEOSERVER_APIKEY')
+  api_request = requests.get("https://meteoserver.nl/api//getij.php?lat=52.4577508&long=4.3891164&key=" + meteoserver_apikey)
+  try:
+    standenlijst = []
+    api_result = json.loads(api_request.content)
+    standenlijst = api_result['jsongetij'][1]['tijden']
+  except Exception as e:
+    api_result = "Error..."
+  return render(request, 'home.html', {'api_result': api_result, 'standenlijst': standenlijst})
 
 # about view
 def about(request):
