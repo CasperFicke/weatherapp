f37c39b5b586f7b4fbcea28cb7ecfe356cb49d27
CasperFicke <cficke@quicknet.nl>
2020-09-14 12:59:35 +0200

    add search funtionality

--- lookup/views.py
+++ lookup/views.py
@@ -7,14 +7,39 @@ def home(request):
   import json
   import requests
   meteoserver_apikey = os.getenv('METEOSERVER_APIKEY')
-  api_request = requests.get("https://meteoserver.nl/api//getij.php?lat=52.4577508&long=4.3891164&key=" + meteoserver_apikey)
-  try:
-    standenlijst = []
-    api_result = json.loads(api_request.content)
-    standenlijst = api_result['jsongetij'][1]['tijden']
-  except Exception as e:
-    api_result = "Error..."
-  return render(request, 'home.html', {'api_result': api_result, 'standenlijst': standenlijst})
+  if request.method == "POST":
+    zipcode = request.POST['zipcode']
+    api_request = requests.get("https://meteoserver.nl/api//getij.php?"+ zipcode + "&key=" + meteoserver_apikey)
+    try:
+      standenlijst = []
+      api_result = json.loads(api_request.content)
+      standenlijst = api_result['jsongetij'][1]['tijden']
+    except Exception as e:
+      api_result = "Error..."
+    
+    if api_result['jsongetij'][0]['plaats'] == "IJmuiden Buitenhaven":
+      category_color = "usg"
+    else:
+      category_color = "good"
+
+    return render(request, 'home.html', {'api_result': api_result, 'category_color': category_color, 'standenlijst': standenlijst})
+  else:
+    # den helder: lat=52.971683&long=4.781498
+    # IJmuiden  : lat=52.4577508&long=4.3891164
+    api_request = requests.get("https://meteoserver.nl/api//getij.php?lat=52.4577508&long=4.3891164&key=" + meteoserver_apikey)
+    try:
+      standenlijst = []
+      api_result = json.loads(api_request.content)
+      standenlijst = api_result['jsongetij'][1]['tijden']
+    except Exception as e:
+      api_result = "Error..."
+    
+    if api_result['jsongetij'][0]['plaats'] == "IJmuiden Buitenhaven":
+      category_color = "usg"
+    else:
+      category_color = "good"
+
+    return render(request, 'home.html', {'api_result': api_result, 'category_color': category_color, 'standenlijst': standenlijst})
 
 # about view
 def about(request):
